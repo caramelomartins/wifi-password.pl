ef4d8e4fb979cc69a10373cd2d0735025a4cc72b
Hugo Martins <caramelo.martins@gmail.com>
2016-07-28 19:43:00 +0100

    run perltidy on files

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 
 [![Build Status](https://travis-ci.org/caramelomartins/wifi-password.pl.svg?branch=master)](https://travis-ci.org/caramelomartins/wifi-password.pl)
 
-Show your wifi password. Fast and easy. Inspired by [wifi-password](https://github.com/rauchg/wifi-password) - shameless rip-off would be more accurate.
+Show your wifi password. Fast and easy. Inspired by
+[wifi-password](https://github.com/rauchg/wifi-password) - shameless rip-off
+would be more accurate.
 
-### Why Perl?
-Other languages could've been more suitable (`bash`...) but I wanted to fiddle with Perl and it comes pre-installed with Ubuntu from start.
+## Why Perl?
+
+Other languages could've been more suitable (`bash`...) but I wanted to fiddle with
+Perl and it comes pre-installed with Ubuntu from start.
 
 For fun, I chose Perl.
 
@@ -14,7 +18,8 @@ For fun, I chose Perl.
 Run the following snippet:
 
 ```shell
-curl -o ~/bin/wifi-password.pl https://raw.githubusercontent.com/caramelomartins/wifi-password.pl/master/wifi-password.pl && chmod +x ~/.bin/wifi-password.pl
+curl -o ~/bin/wifi-password.pl https://raw.githubusercontent.com/caramelomartins/wifi-password.pl/master/wifi-password.pl
+&& chmod +x ~/.bin/wifi-password.pl
 ```
 
 If you don't have `~/bin/` change it to whatever folder you have in your $PATH.
@@ -37,7 +42,8 @@ $ wifi-password.pl [SSID]
 
 ## Requirements
 
-The script relies on the following dependencies - which are usually pre-installed I believe:
+The script relies on the following dependencies - which are usually pre-installed
+I believe:
 
 - [perl 5](https://www.perl.org/)
 - [nmcli](http://linux.die.net/man/1/nmcli)
@@ -48,6 +54,7 @@ The script relies on the following dependencies - which are usually pre-installe
 ## Support
 
 As of this moment it was only tested in the following distributions:
+
 - Ubuntu 16.04
 
 Testing in other distributions would be more than welcome.
@@ -58,7 +65,9 @@ Testing in other distributions would be more than welcome.
 - [x] Easy installation (maybe a bash script).
 
 ## Contribute
+
 Contributions are more than welcome. Just submit a pull request.
 
 ## License
+
 [MIT](https://opensource.org/licenses/MIT)
--- wifi-password.pl
+++ wifi-password.pl
@@ -3,14 +3,13 @@
 use strict;
 use warnings;
 
-my $version = "0.0.2";
-my $networkManager ="/etc/NetworkManager/";
-my $ssid = "";
-my $pwd = "";
+my $version        = "0.0.2";
+my $networkManager = "/etc/NetworkManager/";
+my $ssid           = "";
+my $pwd            = "";
 
 # Help message subroutine.
-sub help()
-{
+sub help() {
     print("\nUsage wifi-password [OPTIONS] [SSID]\n");
     print("\nOptions:\n");
     print(" -v, --version\tOutput version.\n");
@@ -19,44 +18,45 @@ sub help()
 }
 
 # Check dependencies.
-sub checkDependencies()
-{
-    if(`which perl` eq "") { die "ERROR: perl not installed.\n\n" }
-    elsif(`which nmcli` eq "") { die "ERROR: nmcli not installed.\n\n" }
-    elsif(`which cat` eq "") { die "ERROR: cat not installed.\n\n" }
-    elsif(`which grep` eq "") { die "ERROR: grep not intsalled.\n\n" }
-    elsif(`which sed` eq "") { die "ERROR: sed not installed.\n\n" }
+sub checkDependencies() {
+    if    ( `which perl` eq "" )  { die "ERROR: perl not installed.\n\n" }
+    elsif ( `which nmcli` eq "" ) { die "ERROR: nmcli not installed.\n\n" }
+    elsif ( `which cat` eq "" )   { die "ERROR: cat not installed.\n\n" }
+    elsif ( `which grep` eq "" )  { die "ERROR: grep not intsalled.\n\n" }
+    elsif ( `which sed` eq "" )   { die "ERROR: sed not installed.\n\n" }
 }
 
-if ( $#ARGV > 0)
-{
-    help(); exit 1;
+if ( $#ARGV > 0 ) {
+    help();
+    exit 1;
 }
-elsif($#ARGV > -1)
-{
-    if ($ARGV[0] eq "-v") { print "$version\n"; exit 1; }
-    elsif ($ARGV[0] eq "-h") { help(); exit 1; }
-    else { $ssid = $ARGV[0] }
-};
+elsif ( $#ARGV > -1 ) {
+    if    ( $ARGV[0] eq "-v" ) { print "$version\n"; exit 1; }
+    elsif ( $ARGV[0] eq "-h" ) { help();             exit 1; }
+    else                       { $ssid = $ARGV[0] }
+}
 
 print "\n";
 
 checkDependencies();
 
 # Set SSID or use default.
-if( $ssid eq "")
-{
-    $ssid = `nmcli -t -f type,state,connection d | grep wifi | grep connected: | sed "s/^wifi:connected://"`;
-    # http://stackoverflow.com/questions/3931569/how-can-i-remove-all-whitespaces-and-linebreaks-in-perl
+if ( $ssid eq "" ) {
+    $ssid =
+`nmcli -t -f type,state,connection d | grep wifi | grep connected: | sed "s/^wifi:connected://"`;
+
+# http://stackoverflow.com/questions/3931569/how-can-i-remove-all-whitespaces-and-linebreaks-in-perl
     $ssid =~ s/\s+//g;
 
-    unless($ssid ne "") { die "ERROR: SSID couldn't be found. Are you connected to wifi?\n\n"; }
+    unless ( $ssid ne "" ) {
+        die "ERROR: SSID couldn't be found. Are you connected to wifi?\n\n";
+    }
 }
 
 # Verify that a Network connection exists.
 my $path = "$networkManager" . "system-connections/$ssid";
 
-unless( -e $path){
+unless ( -e $path ) {
     die "ERROR: SSID \"$ssid\" is not defined on this machine.\n\n";
 }
 
